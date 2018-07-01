4861b506d30d7a465ea9d1e14bd2bdd61ef78d67
AlexKlym <klemenkoalex95@gmail.com>
2018-07-01 15:26:06 +0200

    ALevelCourses

--- src/practice1/Task2.java
+++ src/practice1/Task2.java
@@ -1,4 +1,22 @@
 package practice1;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = getArray(3, 2, 0, 5, 0, 9, 0, 0, 11, 7, 0);
+
+        for (int b : a) System.out.print(b + " ");
+    }
+
+    private static ArrayList<Integer> getArray(int... nums) {
+        ArrayList<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == 0) b.add(i);
+
+        return b;
+    }
+
+
 }
