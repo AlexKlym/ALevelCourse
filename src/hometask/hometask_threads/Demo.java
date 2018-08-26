32215a55069701761a35b802476d88cfae919d41
AlexKlym <klemenkoalex95@gmail.com>
2018-08-26 16:10:28 +0300

    Commit

---
+++ src/hometask/hometask_threads/Demo.java
@@ -0,0 +1,20 @@
+package hometask.hometask_threads;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputChecker inputChecker = new InputChecker();
+        Thread t = new Thread(inputChecker);
+        t.start();
+        while (true) {
+            System.out.println("Please input the text or type <quit>");
+            String input = scanner.nextLine();
+            inputChecker.addString(input);
+            if (input.equals("quit")) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/hometask/hometask_threads/InputChecker.java
@@ -0,0 +1,50 @@
+package hometask.hometask_threads;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class InputChecker implements Runnable {
+    private String previousInput = "";
+    private String currentInput = "";
+    private String newString = "";
+    private boolean running = true;
+    private StringBuilder sb = new StringBuilder();
+
+    private void terminate() {
+        running = false;
+    }
+
+    public void addString(String s) {
+        sb.append(s).append(System.lineSeparator());
+        newString = s;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            currentInput = sb.toString();
+            try {
+                if (!currentInput.equals(previousInput)) {
+                    write2file();
+                }
+                if (newString.equals("quit")) {
+                    terminate();
+                } else {
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void write2file() {
+        try {
+            Files.write(Paths.get("output.txt"), currentInput.getBytes());
+            previousInput = currentInput;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
